b90ec70b1a618a349fb775dd6de410df689d1a3e
Kyle Wood <kyle.wood@entelect.co.za>
2023-05-04 13:55:27 +0200

    Added Splash screen

---
+++ app/src/main/java/com/om/hackathon/collaborate/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.om.hackathon.collaborate
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.om.hackathon.collaborate.ui.theme.CollaborateTheme
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
+    }
+}
+
