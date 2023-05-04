8960846c4cd88f153328186b08f1a3b22e69b9aa
Allan Chang <allan.chang@entelect.co.za>
2023-05-04 22:02:26 +0200

    Fix import

--- app/src/main/java/com/om/hackathon/collaborate/SplashActivity.kt
+++ app/src/main/java/com/om/hackathon/collaborate/SplashActivity.kt
@@ -3,16 +3,7 @@ package com.om.hackathon.collaborate
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.om.hackathon.collaborate.hustle.DashboardActivity
-import com.om.hackathon.collaborate.ui.theme.CollaborateTheme
+import com.om.hackathon.collaborate.dashboard.DashboardActivity
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
