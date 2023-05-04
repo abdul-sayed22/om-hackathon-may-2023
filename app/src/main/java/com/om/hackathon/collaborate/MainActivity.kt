237e37a5b025166eb90648edcc243d6a499c1f1e
Allan Chang <allan.chang@entelect.co.za>
2023-05-04 15:14:10 +0200

    Add Hustle Card

--- app/src/main/java/com/om/hackathon/collaborate/MainActivity.kt
+++ app/src/main/java/com/om/hackathon/collaborate/MainActivity.kt
@@ -3,13 +3,32 @@ package com.om.hackathon.collaborate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.om.hackathon.collaborate.ui.theme.CollaborateTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,5 +61,41 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     CollaborateTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun HustleCard(name: String, owner: String, @DrawableRes imageId: Int) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(20.dp)) {
+        Card(
+            elevation = CardDefaults.cardElevation( 4.dp)
+        ) {
+            Image(painter = painterResource(id = imageId), contentDescription = null)
+            Column(modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()) {
+                Text(name, fontWeight = FontWeight.W700, color = Color.Black)
+                Text(owner, color = Color.Black)
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End) {
+                    Icon(
+                        Icons.Filled.Favorite,
+                        contentDescription = "Favorite",
+                        tint = Color.Red
+                    )
+                    Icon(
+                        Icons.Filled.Call,
+                        contentDescription = "Call",
+                        tint = Color.Black
+                    )
+                }
+            }
+        }
     }
 }
