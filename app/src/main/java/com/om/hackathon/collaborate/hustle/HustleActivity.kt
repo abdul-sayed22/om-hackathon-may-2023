b36a55dfeedc70a3fb596e5f4d765b06d7f44b1b
Allan Chang <allan.chang@entelect.co.za>
2023-05-05 12:48:16 +0200

    Make column scrollable

--- app/src/main/java/com/om/hackathon/collaborate/hustle/HustleActivity.kt
+++ app/src/main/java/com/om/hackathon/collaborate/hustle/HustleActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.Edit
@@ -60,7 +62,9 @@ class HustleActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(modifier = Modifier.padding(15.dp)) {
+                    Column(modifier = Modifier
+                        .verticalScroll(rememberScrollState())
+                        .padding(15.dp)) {
                         Row(verticalAlignment = Alignment.CenterVertically) {
                             Text(
                                 text = hustle.name,
